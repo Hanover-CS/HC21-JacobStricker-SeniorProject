91b9c7e64e9ac23b292a7cd3dcd8c729c98397e1
Jacob <strickerrj21@hanover.edu>
2020-11-15 18:15:18 -0500

    Added support for the Unity Test Framework and wrote a functioning stub test for the player ship.

---
+++ Assets/Tests/PlayerShip_Tests.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class PlayerShip_Tests
+{
+    [Test]
+    public void PlayerShipTest()
+    {
+        
+    }
+}
